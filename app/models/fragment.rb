fac508d22637517d4d261c9908f2c0a55320a99d
Ryan Kerr <leboshi@gmail.com>
2017-07-29 22:31:14 -0400

    Added Stories

--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,15 +1,15 @@
 class Fragment < ApplicationRecord
-  @@empty_db_message = 'This story is brand new.  Good luck!'
+  belongs_to :story
 
   def self.empty_db_message
-    @@empty_db_message
+    'This story is brand new.  Good luck!'
   end
 
   def previous_line
     if Fragment.any?
       Fragment.last.extract_last_sentence
     else
-      @@empty_db_message
+      empty_db_message
     end
   end
 
